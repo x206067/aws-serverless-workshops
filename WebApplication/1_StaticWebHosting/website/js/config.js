de6d3dc1cc1d5309b7f071932f3f37e0e5e06da2
x206067 <49273686+x206067@users.noreply.github.com>
2019-08-26 12:10:37 +0100

    Update config.js

--- WebApplication/1_StaticWebHosting/website/js/config.js
+++ WebApplication/1_StaticWebHosting/website/js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_TZ7UN376E', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '14r4d0bscjd5of69nqf0mtg8bv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
